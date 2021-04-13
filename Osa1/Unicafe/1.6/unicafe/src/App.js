4bc7d7825c0c9df6738e44333b0abd32e4bbecc2
anjumiel <antti.j.mielonen@student.jyu.fi>
2021-04-13 09:49:19 +0300

    1.11 tehty

--- Osa1/Unicafe/1.6/unicafe/src/App.js
+++ Osa1/Unicafe/1.6/unicafe/src/App.js
@@ -1,29 +1,38 @@
 import React, { useState } from 'react'
 
-const Statistics = (props) => {
-  if (props.kaikki == 0) return (
-    <div>
-    <p>Ei annettua palautetta</p>
-    </div>
-  )
+
+const Stats = (props) => {
+  if (props.kaikki === 0) return (<p>Ei annettua palautetta</p>)
   return (
-      <div>
-        <StatisticsLine text="hyvä" value={props.hyva}/>
-        <StatisticsLine text="neutraali" value={props.neutraali}/>
-        <StatisticsLine text="huono" value={props.huono}/>
-        <StatisticsLine text="kaikki" value={props.kaikki}/>
-        <StatisticsLine text="keskiarvo" value={props.ka}/>
-        <StatisticsLine text="positiivinen" value={props.hyva/props.kaikki}/>
-      </div>
+    <table>
+      <tr>
+        <td>hyvä</td>
+        <td>{props.hyva}</td>
+      </tr>
+      <tr>
+        <td>neutraali</td>
+        <td>{props.neutraali}</td>
+      </tr>
+      <tr>
+        <td>huono</td>
+        <td>{props.huono}</td>
+      </tr>
+      <tr>
+        <td>kaikki</td>
+        <td>{props.kaikki}</td>
+      </tr>
+      <tr>
+        <td>keskiarvo</td>
+        <td>{props.ka}</td>
+      </tr>
+      <tr>
+        <td>positiivinen</td>
+        <td>{props.hyva/props.kaikki}</td>
+      </tr>
+    </table>
   )
 }
 
-
-const StatisticsLine = ({text, value}) => {
-  return (
-    <p>{text} {value}</p>
-  )
-}
 
 const Button = ({funktio, text}) => {
   return (
@@ -56,9 +65,8 @@ const App = (props) => {
       <h1>
       Tilastot
       </h1>
-      <Statistics hyva={hyva} neutraali={neutraali}
+      <Stats hyva={hyva} neutraali={neutraali}
       huono={huono} kaikki={kaikki} ka={ka} />
-
     </div>
   )
 
